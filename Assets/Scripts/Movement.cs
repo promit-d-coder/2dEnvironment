9fb5228b43eff0e699f320ed76901681e09176cf
promit-d-coder <106033706+promit-d-coder@users.noreply.github.com>
2022-10-20 02:38:27 +0530

    Task: Added Jump and Fixed Idle poses
    1. Fixed Idle animation
    2. Added Jump Animation
    3. Added Jump functionality

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,6 +8,13 @@ public enum PlayerState
     walk,
     jump
 }
+
+public enum LastDirection
+{
+    right,
+    left
+}
+
 public class Movement : MonoBehaviour
 {
     //Player walking speed
@@ -17,26 +24,63 @@ public class Movement : MonoBehaviour
     //Player animator
     [SerializeField] private Animator playerAnimator;
     Vector2 vector;
-    private PlayerState pState;
+    [SerializeField] private BoxCollider2D playerCollider;
+    //private PlayerState pState;
+    //private LastDirection lDirection;
+    private bool isGrounded = false;
     // Start is called before the first frame update
     void Start()
     {
-        pState = PlayerState.idle;
+        playerAnimator.SetFloat("lastDirection", 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         vector.x = Input.GetAxisRaw("Horizontal");
-        vector.y = Input.GetAxisRaw("Vertical");
+        //vector.y = Input.GetAxisRaw("Vertical");
 
         playerAnimator.SetFloat("horizontal", vector.x);
-        playerAnimator.SetFloat("vertical", vector.y);
+        playerAnimator.SetFloat("vertical", player2D.velocity.y);
         playerAnimator.SetFloat("speed", vector.sqrMagnitude);
+        if(vector.x > 0f)
+        {
+            playerAnimator.SetFloat("lastDirection", 1f);
+        }
+        else if(vector.x < 0f)
+        {
+            playerAnimator.SetFloat("lastDirection", 0f);
+        }
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                player2D.AddForce(new Vector2(0, 4), ForceMode2D.Impulse);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         player2D.transform.position = player2D.position + vector * walkSpeed * Time.fixedDeltaTime;
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true;
+            playerAnimator.SetBool("isGrounded", true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            isGrounded = false;
+            playerAnimator.SetBool("isGrounded", false);
+        }
+    }
+
 }
